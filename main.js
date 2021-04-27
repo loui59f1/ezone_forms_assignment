47134ba9e377f8708302c1fd23c94bfc7878ec6e
Louise <loui59f1@stud.kea.dk>
2021-04-27 22:32:04 +0200

    Checkboxes and new games popup works

--- main.js
+++ main.js
@@ -11,27 +11,67 @@ function init() {
 
 function showModal() {
     document.querySelector("#modal.step_1").style.display = "block";
+    document.querySelector("#step_bar_1").style.width = "12.5%";
+    //document.querySelector(".btn_step_1").addEventListener("click", step2Modal);
 
-    document.querySelector(".btn_step_1").addEventListener("click", step2Modal);
+    document.querySelector(".btn_step_1").addEventListener("click", () => checkGameCheckboxes("game", step2Modal));
 }
+
+function checkGameCheckboxes(name, callback) {
+    const selected = document.querySelectorAll(`[name=${name}]:checked`);
+    console.log(selected);
+
+    if (selected.length > 0) {
+        callback();
+    } else {
+        alert("Choose at least one game")
+    }
+}
+
 
 function step2Modal() {
     document.querySelector("#modal.step_1").style.display = "none";
     document.querySelector("#modal.step_2").style.display = "block";
+    document.querySelector("#step_bar_2").style.width = "25%";
 
-    document.querySelector(".btn_step_2").addEventListener("click", step3Modal);
+    document.querySelector(".btn_step_2").addEventListener("click", () => checkTypeCheckboxes("type", step3Modal));
 }
+
+function checkTypeCheckboxes(type, callback) {
+    const selected = document.querySelectorAll(`[name=${type}]:checked`);
+    console.log(selected);
+
+    if (selected.length > 0) {
+        callback();
+    } else {
+        alert("Choose at least one type of gameplay")
+    }
+}
+
 
 function step3Modal() {
     document.querySelector("#modal.step_2").style.display = "none";
     document.querySelector("#modal.step_3").style.display = "block";
+    document.querySelector("#step_bar_3").style.width = "37.5%";
 
-    document.querySelector(".btn_step_3").addEventListener("click", step4Modal);
+    document.querySelector(".btn_step_3").addEventListener("click", () => checkAreaCheckboxes("area", step4Modal));
+}
+
+function checkAreaCheckboxes(area, callback) {
+    const selected = document.querySelectorAll(`[name=${area}]:checked`);
+    console.log(selected);
+
+    if (selected.length > 0) {
+        callback();
+    } else {
+        alert("Choose at least one area")
+    }
 }
 
 function step4Modal() {
     document.querySelector("#modal.step_3").style.display = "none";
     document.querySelector("#modal.step_4").style.display = "block";
+    document.querySelector("#step_bar_4").style.width = "50%";
 }
 
 function getJSON() {
@@ -56,20 +96,22 @@ function showCheckboxes(data) {
     let types = data.types;
     let areas = data.areas;
 
-    games.forEach(gameCheckBoxes);
+    games.forEach(gameBoxes);
     types.forEach(typeCheckBoxes);
     areas.forEach(areasCheckBoxes);
 }
 
 //STEP 1
-function gameCheckBoxes(game) {
-    const clone = document.querySelector("template#checkbox_games").content.cloneNode(true);
+function gameBoxes(game) {
+    let gameName = minifyText(game);
+    const clone = document.querySelector("template#games_boxes").content.cloneNode(true);
 
     clone.querySelector("label").textContent = game;
-    clone.querySelector("label").setAttribute("for", game);
+    clone.querySelector("label").setAttribute("for", gameName);
 
-    clone.querySelector("input").setAttribute("id", game);
-    clone.querySelector("input").setAttribute("value", game);
+    clone.querySelector("input").setAttribute("id", gameName);
+    clone.querySelector("input").setAttribute("value", gameName);
+    clone.querySelector("li").style.backgroundImage = "url('" + "img/" + gameName + ".jpg" + "')";
 
     // opsæt regel for specialtegn (cs:go)
 
@@ -78,28 +120,39 @@ function gameCheckBoxes(game) {
 
 //STEP 2
 function typeCheckBoxes(type) {
+    let typeName = minifyText(type);
+
     const clone = document
         .querySelector("template#checkbox_types")
         .content.cloneNode(true);
 
     clone.querySelector("label").textContent = type;
-    clone.querySelector("label").setAttribute("for", type);
+    clone.querySelector("label").setAttribute("for", typeName);
 
-    clone.querySelector("input").setAttribute("id", type);
-    clone.querySelector("input").setAttribute("value", type);
+    clone.querySelector("input").setAttribute("id", typeName);
+    clone.querySelector("input").setAttribute("value", typeName);
 
     document.querySelector("#step_2_controls").appendChild(clone);
 }
 
 //STEP 3
 function areasCheckBoxes(area) {
+    let areaName = minifyText(area);
+
     const clone = document.querySelector("template#checkbox_areas").content.cloneNode(true);
 
     clone.querySelector("label").textContent = area;
-    clone.querySelector("label").setAttribute("for", area);
+    clone.querySelector("label").setAttribute("for", areaName);
 
-    clone.querySelector("input").setAttribute("id", area);
-    clone.querySelector("input").setAttribute("value", area);
+    clone.querySelector("input").setAttribute("id", areaName);
+    clone.querySelector("input").setAttribute("value", areaName);
 
     document.querySelector("#step_3_controls").appendChild(clone);
+}
+
+function minifyText(string) {
+    string = string.toLowerCase();
+    string = string.replaceAll(/[^a-zA-Z ]/g, "");
+    string = string.replaceAll(" ", "");
+    return string;
 }
