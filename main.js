630c3589cc694a4f1559ee1619399add0039edfa
Louise <loui59f1@stud.kea.dk>
2021-04-28 16:11:51 +0200

    Checkbox styling

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ function showModal() {
     document.querySelector(".step_1 .pills").classList.add("fade_pills");
     document.querySelector(".step_1 .pills_2").classList.add("fade_pills");
 
-    document.querySelector(".btn_step_1").addEventListener("click", () => checkGameCheckboxes("game", step3Modal));
+    document.querySelector(".btn_step_1").addEventListener("click", () => checkGameCheckboxes("game", step2Modal));
 }
 
 function checkGameCheckboxes(name, callback) {
@@ -29,15 +29,15 @@ function checkGameCheckboxes(name, callback) {
     }
 }
 
-function step3Modal() {
+function step2Modal() {
     document.querySelector("#modal.step_1").style.display = "none";
-    document.querySelector("#modal.step_3").style.display = "block";
-    document.querySelector("#step_bar_3").classList.add("load_step_2");
-    document.querySelector(".step_3 .column_content").classList.add("slidein");
-    document.querySelector(".step_3 .pills").classList.add("fade_pills_remove");
-    document.querySelector(".step_3 .pills_2").classList.add("fade_pills_remove");
+    document.querySelector("#modal.step_2").style.display = "block";
+    document.querySelector("#step_bar_2").classList.add("load_step_2");
+    document.querySelector(".step_2 .column_content").classList.add("slidein");
+    document.querySelector(".step_2 .pills").classList.add("fade_pills_remove");
+    document.querySelector(".step_2 .pills_2").classList.add("fade_pills_remove");
 
-    document.querySelector(".btn_step_3").addEventListener("click", () => checkAreaCheckboxes("area", step4Modal));
+    document.querySelector(".btn_step_2").addEventListener("click", () => checkAreaCheckboxes("area", step3Modal));
 }
 
 function checkAreaCheckboxes(area, callback) {
@@ -51,13 +51,13 @@ function checkAreaCheckboxes(area, callback) {
     }
 }
 
-function step4Modal() {
-    document.querySelector("#modal.step_3").style.display = "none";
-    document.querySelector("#modal.step_4").style.display = "block";
-    document.querySelector("#step_bar_4").classList.add("load_step_3");
-    document.querySelector(".step_4 .column_content").classList.add("slidein");
-    document.querySelector(".step_4 .pills").classList.add("fade_pills_2");
-    document.querySelector(".step_4 .pills_2").classList.add("fade_pills_2");
+function step3Modal() {
+    document.querySelector("#modal.step_2").style.display = "none";
+    document.querySelector("#modal.step_3").style.display = "block";
+    document.querySelector("#step_bar_3").classList.add("load_step_3");
+    document.querySelector(".step_3 .column_content").classList.add("slidein");
+    document.querySelector(".step_3 .pills").classList.add("fade_pills_2");
+    document.querySelector(".step_3 .pills_2").classList.add("fade_pills_2");
 
     document.querySelector(".submit_btn").addEventListener("click", checkNameValid);
 }
@@ -76,12 +76,7 @@ function getJSON() {
 }
 
 function showCheckboxes(data) {
-    console.log(data.games);
-    console.log(data.types);
-    console.log(data.areas);
-
     let games = data.games;
-    let types = data.types;
     let areas = data.areas;
 
     games.forEach(gameBoxes);
@@ -122,7 +117,7 @@ function gameBoxes(game) {
 //     document.querySelector("#step_2_controls").appendChild(clone);
 // }
 
-//STEP 3
+//STEP 2
 function areasCheckBoxes(area) {
     let areaName = minifyText(area);
 
@@ -134,7 +129,7 @@ function areasCheckBoxes(area) {
     clone.querySelector("input").setAttribute("id", areaName);
     clone.querySelector("input").setAttribute("value", areaName);
 
-    document.querySelector("#step_3_controls").appendChild(clone);
+    document.querySelector("#step_2_controls").appendChild(clone);
 }
 
 function minifyText(string) {
@@ -245,4 +240,13 @@ function post() {
     })
         .then(res => res.json())
         .then(data => console.log(data));
+
+    thankYouPage();
+}
+
+function thankYouPage() {
+    document.querySelector("#modal.step_3").style.display = "none";
+    document.querySelector("#modal.step_4").style.display = "block";
+    document.querySelector(".step_4 .column_content").classList.add("slidein");
+    document.querySelector("#step_bar_4").classList.add("load_step_4");
 }
