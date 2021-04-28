d9b0c0a71faaada3408cf9921c38cd0fa7700420
Louise <loui59f1@stud.kea.dk>
2021-04-28 13:32:34 +0200

    Step 1-2 done

--- main.js
+++ main.js
@@ -11,10 +11,9 @@ function init() {
 
 function showModal() {
     document.querySelector("#modal.step_1").style.display = "block";
-    document.querySelector("#step_bar_1").style.width = "12.5%";
-    //document.querySelector(".btn_step_1").addEventListener("click", step2Modal);
+    document.querySelector("#step_bar_1").classList.add("load_step_1");
 
-    document.querySelector(".btn_step_1").addEventListener("click", () => checkGameCheckboxes("game", step2Modal));
+    document.querySelector(".btn_step_1").addEventListener("click", () => checkGameCheckboxes("game", step3Modal));
 }
 
 function checkGameCheckboxes(name, callback) {
@@ -29,30 +28,36 @@ function checkGameCheckboxes(name, callback) {
 }
 
 
-function step2Modal() {
-    document.querySelector("#modal.step_1").style.display = "none";
-    document.querySelector("#modal.step_2").style.display = "block";
-    document.querySelector("#step_bar_2").style.width = "25%";
+// function step2Modal() {
+//     document.querySelector("#modal.step_1").style.display = "none";
+//     document.querySelector("#modal.step_2").style.display = "block";
+//     document.querySelector("#step_bar_2").classList.add("load_step_2");
+//     document.querySelector(".step_2 .column_content").classList.add("slidein");
+//     document.querySelector(".step_2 .pills").classList.add("fade_pills");
 
-    document.querySelector(".btn_step_2").addEventListener("click", () => checkTypeCheckboxes("type", step3Modal));
-}
+//     // Indholdet af step 2 slide ind fra højre  
 
-function checkTypeCheckboxes(type, callback) {
-    const selected = document.querySelectorAll(`[name=${type}]:checked`);
-    console.log(selected);
+//     document.querySelector(".btn_step_2").addEventListener("click", () => checkTypeCheckboxes("type", step3Modal));
+// }
 
-    if (selected.length > 0) {
-        callback();
-    } else {
-        alert("Choose at least one type of gameplay")
-    }
-}
+// function checkTypeCheckboxes(type, callback) {
+//     const selected = document.querySelectorAll(`[name=${type}]:checked`);
+//     console.log(selected);
+
+//     if (selected.length > 0) {
+//         callback();
+//     } else {
+//         alert("Choose at least one type of gameplay")
+//     }
+// }
 
 
 function step3Modal() {
-    document.querySelector("#modal.step_2").style.display = "none";
+    document.querySelector("#modal.step_1").style.display = "none";
     document.querySelector("#modal.step_3").style.display = "block";
-    document.querySelector("#step_bar_3").style.width = "37.5%";
+    document.querySelector("#step_bar_3").classList.add("load_step_2");
+    document.querySelector(".step_3 .column_content").classList.add("slidein");
+    document.querySelector(".step_3 .pills").classList.add("fade_pills");
 
     document.querySelector(".btn_step_3").addEventListener("click", () => checkAreaCheckboxes("area", step4Modal));
 }
@@ -71,7 +76,11 @@ function checkAreaCheckboxes(area, callback) {
 function step4Modal() {
     document.querySelector("#modal.step_3").style.display = "none";
     document.querySelector("#modal.step_4").style.display = "block";
-    document.querySelector("#step_bar_4").style.width = "50%";
+    document.querySelector("#step_bar_4").classList.add("load_step_3");
+    document.querySelector(".step_4 .column_content").classList.add("slidein");
+    document.querySelector(".step_4 .pills").classList.add("fade_pills");
+
+    document.querySelector(".submit_btn").addEventListener("click", checkNameValid);
 }
 
 function getJSON() {
@@ -97,7 +106,6 @@ function showCheckboxes(data) {
     let areas = data.areas;
 
     games.forEach(gameBoxes);
-    types.forEach(typeCheckBoxes);
     areas.forEach(areasCheckBoxes);
 }
 
@@ -119,21 +127,21 @@ function gameBoxes(game) {
 }
 
 //STEP 2
-function typeCheckBoxes(type) {
-    let typeName = minifyText(type);
+// function typeCheckBoxes(type) {
+//     let typeName = minifyText(type);
 
-    const clone = document
-        .querySelector("template#checkbox_types")
-        .content.cloneNode(true);
+//     const clone = document
+//         .querySelector("template#checkbox_types")
+//         .content.cloneNode(true);
 
-    clone.querySelector("label").textContent = type;
-    clone.querySelector("label").setAttribute("for", typeName);
+//     clone.querySelector("label").textContent = type;
+//     clone.querySelector("label").setAttribute("for", typeName);
 
-    clone.querySelector("input").setAttribute("id", typeName);
-    clone.querySelector("input").setAttribute("value", typeName);
+//     clone.querySelector("input").setAttribute("id", typeName);
+//     clone.querySelector("input").setAttribute("value", typeName);
 
-    document.querySelector("#step_2_controls").appendChild(clone);
-}
+//     document.querySelector("#step_2_controls").appendChild(clone);
+// }
 
 //STEP 3
 function areasCheckBoxes(area) {
@@ -155,4 +163,107 @@ function minifyText(string) {
     string = string.replaceAll(/[^a-zA-Z ]/g, "");
     string = string.replaceAll(" ", "");
     return string;
+}
+
+function checkNameValid() {
+    // check først name - hvis ikke valid, vis meddelse
+    if (document.querySelector("#name").checkValidity()) {
+        console.log("Yes name is valid");
+        document.querySelector(".name_error").style.visibility = "hidden";
+        checkGamertagValid();
+    } else {
+        console.log("no name is not valid");
+        document.querySelector(".name_error").style.visibility = "visible";
+    }
+}
+
+function checkGamertagValid() {
+    // check først name - hvis ikke valid, vis meddelse
+    if (document.querySelector("#gamertag").checkValidity()) {
+        console.log("Yes gamertag is valid");
+        document.querySelector(".gamertag_error").style.visibility = "hidden";
+        checkEmailValid();
+    } else {
+        console.log("no gamertag is not valid");
+        document.querySelector(".gamertag_error").style.visibility = "visible";
+    }
+}
+
+function checkEmailValid() {
+    // check først name - hvis ikke valid, vis meddelse
+    if (document.querySelector("#email").checkValidity()) {
+        console.log("Yes email is valid");
+        document.querySelector(".email_error").style.visibility = "hidden";
+        checkPasswordValid();
+    } else {
+        console.log("no email is not valid");
+        document.querySelector(".email_error").style.visibility = "visible";
+    }
+}
+
+function checkPasswordValid() {
+    // check først name - hvis ikke valid, vis meddelse
+    if (document.querySelector("#password").checkValidity()) {
+        console.log("Yes password is valid");
+        document.querySelector(".password_error").style.visibility = "hidden";
+        post();
+    } else {
+        console.log("no password is not valid");
+        document.querySelector(".password_error").style.visibility = "visible";
+        document.querySelector("#password").classList.add("invalid");
+    }
+}
+
+// Klik på submit --> post
+function post() {
+    console.log("Test")
+
+    // Find info fra indtastede data
+    let fullname = document.querySelector("#name").value;
+    let gamertag = document.querySelector("#gamertag").value;
+    let mail = document.querySelector("#email").value;
+    let password = document.querySelector("#password").value;
+
+    // Find checkboxes data
+    let gamesArray = [];
+    let games = document.querySelectorAll(`input[name="game"]:checked`);
+
+    // For each game checked, get label name
+    for (let y = 0; y < games.length; y++) {
+        let label_name = games[y].nextElementSibling.textContent;
+        gamesArray.push(label_name);
+    }
+
+    // Find checkboxes data
+    let areasArray = [];
+    let areas = document.querySelectorAll(`input[name="area"]:checked`);
+
+    // For each area checked, get label name of area
+    for (let y = 0; y < areas.length; y++) {
+        let label_area = areas[y].nextElementSibling.textContent;
+        areasArray.push(label_area);
+    }
+
+    const data = {
+        games: gamesArray,
+        areas: areasArray,
+        name: fullname,
+        email: mail,
+        gamertag: gamertag,
+        password: password,
+    };
+
+    const postData = JSON.stringify(data);
+
+    fetch("https://ezoneproject-d9d3.restdb.io/rest/videogames", {
+        method: "post",
+        headers: {
+            "Content-Type": "application/json; charset=utf-8",
+            "x-apikey": "608672e428bf9b609975a6e2",
+            "cache-control": "no-cache",
+        },
+        body: postData,
+    })
+        .then(res => res.json())
+        .then(data => console.log(data));
 }
